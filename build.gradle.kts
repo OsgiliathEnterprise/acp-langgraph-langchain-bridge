88bc28789987e72aa6f3872acece0651db1a5e9c
Charlie Mordant <cmordant1@gmail.com>
2026-03-14 08:58:23 +0100

    add badges

--- build.gradle.kts
+++ build.gradle.kts
@@ -233,7 +233,7 @@ sonar {
         )
         property(
             "sonar.java.test.binaries",
-            "${layout.buildDirectory.get()}/classes/java/test,${layout.buildDirectory.get()}/classes/kotlin/test"
+            "${layout.buildDirectory.get()}/classes/java/test"
         )
     }
 }
