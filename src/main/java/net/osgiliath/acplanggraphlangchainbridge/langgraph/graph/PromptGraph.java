2c20acf25e28b5a8294161a90b7eec8fbf33d3e9
Charlie Mordant <cmordant1@gmail.com>
2026-03-14 17:54:36 +0100

    chore (quality): improve sonarqube coverage

--- src/main/java/net/osgiliath/acplanggraphlangchainbridge/langgraph/graph/PromptGraph.java
+++ src/main/java/net/osgiliath/acplanggraphlangchainbridge/langgraph/graph/PromptGraph.java
@@ -7,12 +7,13 @@ import org.bsc.langgraph4j.state.AgentState;
 /**
  * Interface for defining a prompt graph. A prompt graph is a specific type of graph that represents the flow of a prompt in the system.
  * Implementing classes should provide the logic to build the graph according to the requirements of the prompt.
+ * @param <S> the type of the state that this graph will use. It must extend AgentState, which is a common base class for states in the LangGraph framework.
  */
-public interface PromptGraph<State extends AgentState> {
+public interface PromptGraph<S extends AgentState> {
     /**
      * Builds the graph for the prompt. This method is called by the framework when the graph is being initialized.
      * @return the built graph
      * @throws GraphStateException if there is an error building the graph, such as invalid node configuration or state issues
      */
-    StateGraph<State> buildGraph() throws GraphStateException;
+    StateGraph<S> buildGraph() throws GraphStateException;
 }
--- src/main/java/net/osgiliath/acplanggraphlangchainbridge/langgraph/serializer/AcpLangChain4jStateSerializer.java
+++ src/main/java/net/osgiliath/acplanggraphlangchainbridge/langgraph/serializer/AcpLangChain4jStateSerializer.java
@@ -8,15 +8,15 @@ import org.bsc.langgraph4j.state.AgentStateFactory;
 
 /**
  * Custom state serializer for ACP that extends the standard LC4jStateSerializer. It overrides the default content serializer to use a custom implementation
- * @param <STATE> the type of the state that this serializer will handle. It must extend AgentState, which is a common base class for states in the LangGraph framework.
+ * @param <S> the type of the state that this serializer will handle. It must extend AgentState, which is a common base class for states in the LangGraph framework.
  */
-public class AcpLangChain4jStateSerializer<STATE extends AgentState> extends LC4jStateSerializer<STATE> {
+public class AcpLangChain4jStateSerializer<S extends AgentState> extends LC4jStateSerializer<S> {
 
     /**
      * Constructor for AcpLangChain4jStateSerializer.
      * @param stateFactory a factory function that creates new instances of the state. This is used by the serializer to create new state instances during deserialization.
      */
-    public AcpLangChain4jStateSerializer(AgentStateFactory<STATE> stateFactory) {
+    public AcpLangChain4jStateSerializer(AgentStateFactory<S> stateFactory) {
         super(stateFactory);
         mapper().unregister(ContentSerializer.class);
         mapper().register(Content.class, new AcpBridgeContentSerializer());
